0dc763c044bda0c1501ed504299b421a833eb170
Sumit Kumar <infosumitkumar3322@gmail.com>
2025-10-14 00:16:59 +0530

    fix and and

--- user-service/src/index.js
+++ user-service/src/index.js
@@ -121,7 +121,7 @@ mongoose
     w: "majority",
   })
   .then(() => {
-    console.log("✅ Connected to MongoDB Atlas");
+    console.log("✅ Connecte dddfd to MongoDB Atlas");
     app.listen(PORT, () => {
       console.log(`🚀 User Service running on port ${PORT}`);
       console.log(
